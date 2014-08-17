a9f22d46a51e938d11478f2b018c961ad5e436b3
Slawomir Smiechura <slawomir@soundcloud.com>
2014-08-17 12:52:57 +0200

    Added missing files

---
+++ lib/dex2jar/command.rb
@@ -0,0 +1,27 @@
+module Dex2jar
+  class Command
+    def initialize(options, files)
+      @options = options
+      @files   = files
+    end
+
+    def execute
+      `#{dex2jar_command} #{options} #{files.join(' ')}`
+    end
+
+    private
+    attr_reader :options, :files
+
+    def dex2jar_command
+      if (windows_environment?)
+        'bin/dex2jar-0.0.9.15/d2j-dex2jar.bat'
+      else
+        'bin/dex2jar-0.0.9.15/d2j-dex2jar.sh'
+      end
+    end
+
+    def windows_environment?
+      (RbConfig::CONFIG['host_os'] =~ /cygwin|mswin|mingw|bccwin|wince|emx/)
+    end
+  end
+end
---
+++ spec/lib/command_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Dex2jar::Command do
+  let(:command) {described_class.new('', [])}
+
+  describe "execute" do
+
+    it 'executes with the right params' do
+      expect(command).to receive(:dex2jar_command).and_return('dex2jar.sh')
+      expect(command).to receive(:`).with('dex2jar.sh  ').and_return("mmr")
+
+      command.execute
+    end
+
+    it 'executes dex2jar.bat on windows environment' do
+      expect(command).to receive(:windows_environment?).and_return(true)
+
+      expect(command.send(:dex2jar_command)).to eql('bin/dex2jar-0.0.9.15/d2j-dex2jar.bat')
+    end
+
+    it 'executes dex2jar.sh on unix environment' do
+      expect(command).to receive(:windows_environment?).and_return(false)
+
+      expect(command.send(:dex2jar_command)).to eql('bin/dex2jar-0.0.9.15/d2j-dex2jar.sh')
+    end
+  end
+end
